c6c93d9bf0957a780bc63e67494c0075b7035e59
rosiegracejones <rosiegracejones@gmail.com>
2023-04-15 19:10:29 +0100

    BlogPost initial code written, plan created

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,22 @@
+import React, { useState } from "react";
+import BlogPost from "../BlogPost";
+import Comment from "../Comment";
+
 function App() {
-  return null;
+  const blog = {
+    title: "Introduction to React",
+    author: "Rosie Jones",
+    datePosted: "15/04/2023",
+    content: `It's been a week learning React. 
+    Props can be confusing, but it's a good job you have .maps to steer you in the right direction.`,
+    imageSrc: "https://tsh.io/wp-content/uploads/2019/12/react-meme1_.png",
+    imageAlt: "frontend vs frontend with react meme",
+  };
+
+  return (
+
+  <BlogPost blog={blog} />
+  )
 }
 
 export default App;
--- src/components/BlogPost/index.js
+++ src/components/BlogPost/index.js
@@ -1,5 +1,19 @@
-function BlogPost() {
-  return null;
+import React from "react";
+
+// enter 'blog' object from App as prop
+// JSX - set each blog item as element in a blogpost div
+// not calling any other components
+
+function BlogPost({ blog }) {
+  return (
+    <div id="blogpost">
+      <h2>{blog.title}</h2>
+      <h3>by {blog.author}</h3>
+      <p id="postdate">{blog.date}</p>
+      <img src={blog.imageSrc} alt={blog.imageAlt} />
+      <p>{blog.content}</p>
+    </div>
+  );
 }
 
 export default BlogPost;
